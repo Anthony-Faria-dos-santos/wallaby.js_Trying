ed235a449ea216d3e675c9c1f2ac977c992ebf93
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2023-12-29 18:41:45 +0100

    init : project with wallaby.js

---
+++ exemple.js
@@ -0,0 +1,49 @@
+
+
+  <!-- Test de functions age prompt-->
+
+// age = window.prompt("entrer votre âge :");
+
+// agePrompt(); 
+
+// function agePrompt() {
+//     if (!age) {
+
+//         alert("Vous n'avez pas répondu à la question");
+
+//     } else {
+
+//         if (age > 0 && age < 18) {
+//             alert("Vous êtes mineur");
+
+//         } else {
+
+//             if ((age % 2 === 0)) {
+//                 alert("Votre réponse doit être un nombre entier !!");
+
+//             } else {
+//                 alert("Vous êtes majeur");
+//             }
+
+
+//         }
+//     };
+// }
+
+
+
+const direBonjour = (prenom, nom) => {
+   let test = ("Bonjour  "+ prenom + " " + nom + " !" + " comment vas-tu ?");
+  return test;
+};
+
+
+
+alert(direBonjour("Anthony", "Faria dos santos")); 
+
+panel = 123; 
+
+// crieDeGuerre = () => {
+//     let crie = "Toujours plus fort !";
+//     return crie;
+// }
